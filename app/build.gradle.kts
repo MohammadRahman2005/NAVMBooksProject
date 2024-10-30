e6e5fa29bf4fe2f8352a0709e03dcdca9f0a9891
Noah_gelinas <noahgelinas1993@gmail.com>
2024-10-30 19:16:51 -0400

    Add Book and Chapter java class to begin html parsing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,6 @@ android {
             useSupportLibrary = true
         }
     }
-
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -47,6 +46,7 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
 }
 
 dependencies {
--- app/src/main/java/com/example/navmbooks/MainActivity.kt
+++ app/src/main/java/com/example/navmbooks/MainActivity.kt
@@ -16,6 +16,7 @@ import com.example.navmbooks.ui.theme.NAVMBooksTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Book.print()
         enableEdgeToEdge()
         setContent {
             NAVMBooksTheme {
