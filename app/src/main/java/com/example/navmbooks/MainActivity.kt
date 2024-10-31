7768bc72af498692a2adc35944f8e3f2e6b48850
MohammadRahman <rahmanmahbub0529@gmail.com>
2024-10-30 21:23:12 -0400

    Set up empty navigation function with top and bottom bar

--- app/src/main/java/com/example/navmbooks/MainActivity.kt
+++ app/src/main/java/com/example/navmbooks/MainActivity.kt
@@ -4,14 +4,44 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.example.navmbooks.ui.theme.NAVMBooksTheme
+import java.util.Locale
+import java.util.Locale.US
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,12 +49,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NAVMBooksTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                BookReadingApp(locale = US)
             }
         }
     }
@@ -45,3 +70,90 @@ fun GreetingPreview() {
         Greeting("Android")
     }
 }
+
+@Composable
+fun BookReadingApp(
+    navController: NavHostController = rememberNavController(),
+    locale: Locale
+) {
+
+    Scaffold(
+        topBar = { NAVMAppBar( navigateUp = { navController.navigateUp() }) },
+        content = {padding ->
+            Column(Modifier.padding(padding)) {
+                NavigationHost(navController = navController)
+            } },
+        bottomBar = {  }
+    )
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NAVMAppBar(
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        title = {
+            Text(
+                stringResource(R.string.app_name),
+                style = MaterialTheme.typography.headlineLarge,
+                modifier = Modifier.padding(top = 8.dp, start = 72.dp)
+            )
+        },
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        ),
+        modifier = modifier,
+        navigationIcon = {
+            IconButton(onClick = navigateUp) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = stringResource(R.string.back_button)
+                )
+            }
+        },
+        actions = {
+            Logo(modifier = Modifier.size(48.dp))
+        }
+    )
+}
+
+@Composable
+fun NavigationHost(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = NavRoutes.HomeScreen.route,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        composable(route = NavRoutes.HomeScreen.route) {
+
+        }
+        composable(route = NavRoutes.LibraryScreen.route) {
+
+        }
+        composable(route = NavRoutes.SearchScreen.route) {
+
+        }
+        composable(route = NavRoutes.ContentScreen.route) {
+
+        }
+        composable(route = NavRoutes.ReadingScreen.route) {
+
+        }
+    }
+}
+
+@Composable
+fun Logo(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(R.drawable.placeholder),
+        contentDescription = "App Logo",
+        modifier = modifier,
+        contentScale = ContentScale.Crop
+    )
+}
+
+
+
