437e420c9c0c3cd1c61e36b1f7326724cc46f698
Rahman Mohammad Mahbub <rahmanmahbub0529@gmail.com>
2024-11-01 16:20:39 +0000

    Revert "Merge branch 'ReadingMode' into 'main'"
    This reverts merge request !2

--- app/src/main/java/com/example/navmbooks/BarItem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.navmbooks
-
-import androidx.compose.ui.graphics.vector.ImageVector
-
-data class BarItem(
-    val title: String,
-    val image: ImageVector,
-    val route: String
-)
--- app/src/main/java/com/example/navmbooks/MainActivity.kt
+++ app/src/main/java/com/example/navmbooks/MainActivity.kt
@@ -1,241 +1,47 @@
 package com.example.navmbooks
 
-import ContentScreen
-import HomeScreen
-import LibraryScreen
-import ReadingScreen
-import SearchScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Info
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.PlayArrow
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
-import androidx.navigation.NavGraph.Companion.findStartDestination
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.navmbooks.ui.theme.NAVMBooksTheme
-import java.util.Locale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            var isReadingMode by rememberSaveable { mutableStateOf(false) }
             NAVMBooksTheme {
-                BookReadingApp(
-                    locale = Locale.US,
-                    isReadingMode = isReadingMode,
-                    onReadingModeChanged = { isReadingMode = it }
-                )
-            }
-        }
-    }
-}
-
-@Composable
-fun BookReadingApp(
-    navController: NavHostController = rememberNavController(),
-    locale: Locale,
-    isReadingMode: Boolean,
-    onReadingModeChanged: (Boolean) -> Unit
-) {
-    val backStackEntry by navController.currentBackStackEntryAsState()
-    val currentRoute = backStackEntry?.destination?.route
-    Scaffold(
-        topBar = {
-            if (currentRoute != NavRoutes.ContentScreen.route && currentRoute != NavRoutes.ReadingScreen.route) {
-                NAVMAppBar(navigateUp = { navController.navigateUp() })
-            }
-        },
-        content = { padding ->
-            Column(Modifier.padding(padding)) {
-                NavigationHost(navController = navController, onReadingModeChanged = onReadingModeChanged)
-            }
-        },
-        bottomBar = {
-            if (!isReadingMode) {
-                BottomNavigationBar(navController = navController)
-            }
-        }
-    )
-}
-
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun NAVMAppBar(
-    navigateUp: () -> Unit,
-    modifier: Modifier = Modifier
-) {
-    TopAppBar(
-        title = {
-            Text(
-                stringResource(R.string.app_name),
-                style = MaterialTheme.typography.headlineLarge,
-                modifier = Modifier.padding(top = 8.dp, start = 50.dp)
-            )
-        },
-        colors = TopAppBarDefaults.mediumTopAppBarColors(
-            containerColor = MaterialTheme.colorScheme.primaryContainer
-        ),
-        modifier = modifier,
-        navigationIcon = {
-            IconButton(onClick = navigateUp) {
-                Icon(
-                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                    contentDescription = stringResource(R.string.back_button)
-                )
-            }
-        },
-        actions = {
-            Logo(modifier = Modifier.size(48.dp))
-        }
-    )
-}
-
-@Composable
-fun Logo(modifier: Modifier = Modifier) {
-    Image(
-        painter = painterResource(R.drawable.placeholder),
-        contentDescription = "App Logo",
-        modifier = modifier,
-        contentScale = ContentScale.Crop
-    )
-}
-
-@Composable
-fun NavigationHost(
-    navController: NavHostController,
-    onReadingModeChanged: (Boolean) -> Unit
-) {
-    NavHost(
-        navController = navController,
-        startDestination = NavRoutes.HomeScreen.route,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        composable(route = NavRoutes.HomeScreen.route) {
-            HomeScreen(navController = navController)
-        }
-        composable(route = NavRoutes.LibraryScreen.route) {
-            LibraryScreen(navController = navController)
-        }
-        composable(route = NavRoutes.SearchScreen.route) {
-            SearchScreen(navController = navController)
-        }
-        composable(route = NavRoutes.ContentScreen.route) {
-            ContentScreen(navController = navController)
-        }
-        composable(route = NavRoutes.ReadingScreen.route) {
-            ReadingScreen(
-                navController = navController,
-                onReadingModeChanged = onReadingModeChanged
-            )
-        }
-    }
-}
-
-
-@Composable
-fun BottomNavigationBar(navController: NavHostController) {
-    NavigationBar {
-        val backStackEntry by navController.currentBackStackEntryAsState()
-        val currentRoutes = backStackEntry?.destination?.route
-
-        navBarItems().forEach { navItem ->
-            NavigationBarItem(
-                selected = currentRoutes == navItem.route,
-                onClick = {
-                    navController.navigate(navItem.route) {
-                        popUpTo(navController.graph.findStartDestination().id) {
-                            saveState = true
-                        }
-                        launchSingleTop = true
-                        restoreState = true
-                    }
-                },
-
-                icon = {
-                    Icon(
-                        imageVector = navItem.image,
-                        contentDescription = navItem.title
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
                     )
-                },
-                label = {
-                    Text(text = navItem.title)
-                },
-            )
+                }
+            }
         }
     }
 }
 
 @Composable
-fun navBarItems() : List<BarItem> {
-    val  barItems = listOf(
-        BarItem(
-            title = stringResource(id = R.string.home),
-            image = Icons.Filled.Home,
-            route = stringResource(id = R.string.home_screen)
-        ),
-        BarItem(
-            title = stringResource(id = R.string.library),
-            image = Icons.Filled.Menu,
-            route = stringResource(id = R.string.library_screen)
-        ),
-        BarItem(
-            title = stringResource(id = R.string.search),
-            image = Icons.Filled.Search,
-            route = stringResource(id = R.string.search_screen)
-        ),
-        BarItem(
-            title = stringResource(id = R.string.content),
-            image = Icons.Filled.PlayArrow,
-            route = stringResource(id = R.string.content_screen)
-        ),
-        BarItem(
-            title = stringResource(id = R.string.reading),
-            image = Icons.Filled.Info,
-            route = stringResource(id = R.string.reading_screen)
-        )
-
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
     )
-
-    return barItems
 }
 
-
-
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NAVMBooksTheme {
+        Greeting("Android")
+    }
+}
