febf07fb7cacdb082b903fd2c8d06a82c28b0555
MohammadRahman <rahmanmahbub0529@gmail.com>
2024-11-01 21:41:14 -0400

    Implement ViewModel for reading mode

--- app/src/main/java/com/example/navmbooks/BookViewModel.kt
+++ app/src/main/java/com/example/navmbooks/BookViewModel.kt
@@ -1,7 +1,15 @@
 package com.example.navmbooks
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 
 class BookViewModel : ViewModel() {
-    
+
+    private var _isReadingMode = false
+    val isReadingMode : Boolean get () = _isReadingMode
+
+    fun toggleReadingMode(isEnabled: Boolean) {
+        _isReadingMode = isEnabled
+    }
 }
