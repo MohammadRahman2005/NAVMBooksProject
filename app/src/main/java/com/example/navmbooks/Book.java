1a81dd8d00737f73078b77c8fa53525929c57593
Noah_gelinas <noahgelinas1993@gmail.com>
2024-11-18 19:12:53 -0500

    Change book class to use new items for image and text, Also removed unused read by Url method

--- app/src/main/java/com/example/navmbooks/Book.java
+++ app/src/main/java/com/example/navmbooks/Book.java
@@ -36,7 +36,7 @@ public class Book {
         this.allContent = String.valueOf(allContent);
         this.coverImage = coverImage;
     }
-    public static Book readBookFromFile(File file, File cover) throws IOException {
+    public static Book readBookFromFile(File file, File cover, String bookDirectory) throws IOException {
         String title="";
         String author="";
         StringBuilder allContent= new StringBuilder();
@@ -54,21 +54,21 @@ public class Book {
                 if (e.children().isEmpty()){
                     allContent.append(e.text()).append('\n');
                     if (chapter!=null) chapter.addContent(new TextItem(e.text()));
-                }else if(e.child(0).tagName().equals("img")){
-                    allContent.append("image here").append("\n");
-                    if (chapter!=null) chapter.addContent(new ImageItem(e.text()));
-                }else if(e.child(0).tagName().equals("table")) {
-                    allContent.append("table here").append("\n");
-                }
-            }
-            if (e.tagName().equals("div")){
-                for (Element child : e.children()){
-                    if (child.tagName().equals("img")){
-                        allContent.append(child.attr("src"));
-                        allContent.append("image here").append("\n");
+                }else {
+                    for (Element child : e.children()) {
+                        if (child.tagName().equals("img")) {
+                            String imgSrc = bookDirectory + File.separator + child.attr("src");
+                            allContent.append("image here").append("\n");
+                            if (chapter != null) chapter.addContent(new ImageItem(imgSrc));
+                        }
+                    }
+                    if (e.child(0).tagName().equals("table")) {
+                        allContent.append("table here").append("\n");
+                        if (chapter != null) chapter.addContent(new TextItem("Table here"));
                     }
                 }
-            }if(e.tagName().equals("table")) {
+            }
+            if(e.tagName().equals("table")) {
                 allContent.append("table here").append("\n");
             }
             if(e.tagName().equals("h2") || e.tagName().equals("h3")){
@@ -77,56 +77,13 @@ public class Book {
                 i++;
                 chapters.add(chapter);
             }else if(e.tagName().equals("p") && chapter!=null){
-                chapter.setText(e.text());
                 if (e.children().isEmpty() || e.child(0).equals("i")){
-                    chapter.setText(e.text().trim()+" ");
+                    chapter.addContent(e.text().trim()+" ");
                 }
+                chapter.addContent(new TextItem(e.text()));
             }
         }
         return new Book(title, author, chapters, allContent, cover);
-    }
-
-    public static Book readBookURL(String URL) throws IOException {
-        String title="";
-        String author="";
-        StringBuilder allContent= new StringBuilder();
-        ArrayList<Chapter> chapters = new ArrayList<Chapter>();
-        Document doc = Jsoup.connect(URL).get();
-        title=doc.getElementsByTag("h1").get(0).text();
-        author = Objects.requireNonNull(doc.getElementById("pg-header-authlist")).getElementsByTag("p").get(0).text();
-
-        Chapter chapter = null;
-        Elements body = doc.body().children();
-        Integer i=1;
-        for (Element e:body) {
-            allContent.append(e.text()).append('\n');
-            if(e.tagName().equals("div")) {
-                if (e.children().isEmpty()){
-                    allContent.append(e.text()).append('\n');
-                }else if(e.child(0).tagName().equals("img")){
-                    allContent.append("image here").append("\n");
-                }else if(e.child(0).tagName().equals("table")) {
-                    allContent.append("table here").append("\n");
-                }
-            }
-            if (e.tagName().equals("img")){
-                allContent.append("image here").append("\n");
-            }if(e.tagName().equals("table")) {
-                allContent.append("table here").append("\n");
-            }
-            if(e.tagName().equals("h2") || e.tagName().equals("h3")){
-                String chapterTitle = e.text();
-                chapter = new Chapter(chapterTitle,i);
-                i++;
-                chapters.add(chapter);
-            }else if(e.tagName().equals("p") && chapter!=null){
-                chapter.setText(e.text());
-                if (e.children().isEmpty() || e.child(0).equals("i")){
-                    chapter.setText(e.text().trim()+" ");
-                }
-            }
-        }
-        return new Book(title, author, chapters, allContent, null );
     }
     @Override
     public String toString(){
