b8b8062314012b6ea77c5f050a67c73d95252fe3
Noah_gelinas <noahgelinas1993@gmail.com>
2024-11-18 19:13:31 -0500

    Add variable that stores currentBookDirectory to pass into readBook Method to add to the image path

--- app/src/main/java/com/example/navmbooks/BookViewModel.kt
+++ app/src/main/java/com/example/navmbooks/BookViewModel.kt
@@ -22,6 +22,8 @@ class BookViewModel(private val repository: FileRepository) : ViewModel() {
     private val _directoryContents = MutableLiveData<List<String>>()
     val directoryContents: LiveData<List<String>> = _directoryContents
 
+    private var currentBookDirectory: String? = null
+
     // Function to set up file download
     fun setupDownload(url: String) {
         viewModelScope.launch(Dispatchers.IO) {
@@ -34,6 +36,7 @@ class BookViewModel(private val repository: FileRepository) : ViewModel() {
                 if (file.exists() && file.length() > 0) {
                     updateDirectoryContents(destDir)
                     val destDirFile = repository.createFile(destDir, fileName.substringBeforeLast("."))
+                    currentBookDirectory = destDirFile.absolutePath
                     UnzipUtils.unzip(file, destDirFile.absolutePath)
                 } else {
                     Log.e("DownloadViewModel", "File is empty or does not exist.")
@@ -70,12 +73,13 @@ class BookViewModel(private val repository: FileRepository) : ViewModel() {
                 val files = repository.context.resources.getStringArray(R.array.booksFile)
                 val coverImages = repository.context.resources.getStringArray(R.array.booksCover)
                 var i = 0;
-                files.forEach {file ->
+                files.forEachIndexed {index, file ->
                     val htmlFile = File(repository.context.getExternalFilesDir(null), file)
                     val cover  = File(repository.context.getExternalFilesDir(null), coverImages[i])
                     i++
+                    currentBookDirectory = htmlFile.parent
                     if (htmlFile.exists() && cover.exists()) {
-                        val book = Book.readBookFromFile(htmlFile, cover)
+                        val book = Book.readBookFromFile(htmlFile, cover, currentBookDirectory!!)
                         bookList= bookList + book
                     }else{
                         Log.e("BookViewModel", "The HTML file does not exist at $htmlFile")
