6e5d4f703d6d887731a9a287fccf8887c3ca1188
Noah_gelinas <noahgelinas1993@gmail.com>
2024-11-26 11:10:55 -0500

    Add table item

---
+++ app/src/main/java/com/example/navmbooks/data/TableItem.java
@@ -0,0 +1,12 @@
+package com.example.navmbooks.data;
+
+public class TableItem {
+    private final String text;
+    public TableItem(String text){
+        this.text=text;
+    }
+
+    public String getText(){
+        return text;
+    }
+}
