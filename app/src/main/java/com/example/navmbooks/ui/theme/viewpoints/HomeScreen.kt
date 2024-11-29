52da75c20b5e355d8a0202dde44ecfa4fadf89e3
enymu <vinsonchiubusiness@gmail.com>
2024-11-29 10:49:21 -0500

    Remove welcome image

--- app/src/main/java/com/example/navmbooks/ui/theme/viewpoints/HomeScreen.kt
+++ app/src/main/java/com/example/navmbooks/ui/theme/viewpoints/HomeScreen.kt
@@ -1,12 +1,10 @@
 package com.example.navmbooks.ui.theme.viewpoints
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -16,11 +14,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.navmbooks.R
 
@@ -47,21 +42,10 @@ fun HomeScreen(
         ) {
             Spacer(modifier = Modifier.height(dimensionResource(R.dimen.big_padding)))
 
-            Image(
-                painter = painterResource(id = R.drawable.welcome),
-                contentDescription = stringResource(R.string.welcome_image_desc),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(200.dp),
-                contentScale = ContentScale.Crop
-            )
-
-            Spacer(modifier = Modifier.height(dimensionResource(R.dimen.big_padding)))
-
             Text(
                 text = stringResource(R.string.welcome_title),
-                style = MaterialTheme.typography.headlineMedium,
-                color = MaterialTheme.colorScheme.primary,
+                style = MaterialTheme.typography.headlineLarge,
+                color = MaterialTheme.colorScheme.primary
             )
 
             Spacer(modifier = Modifier.height(dimensionResource(R.dimen.medium_padding)))
